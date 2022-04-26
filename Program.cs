a5bc6f1251d317ac7673bdbead8654ec2373453a
Felipe Muniz <felipe.muniz@rethink.dev>
2022-04-26 19:13:57 -0400

    Initial commit 2

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace padroes_de_projeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
